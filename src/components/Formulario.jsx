d10f2253996e6d33c827106727ca262d6d8e6ff8
ChemaJob <chema.job.24@gmail.com>
2023-07-31 12:05:06 -0600

    Administracion de pacientes - Creando un Input en el Formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -9,8 +9,15 @@ const Formulario = () => {
         <span className="text-indigo-600 font-bold">Administralos</span>
       </p>
 
-      <form>
-        
+      <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+        <div>
+          <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">Nombre Mascota</label>
+          <input 
+          id="mascota"
+          type="text" 
+          placeholder="Nombre de la mascota" 
+          className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" />
+        </div>
       </form>
       
     </div>
