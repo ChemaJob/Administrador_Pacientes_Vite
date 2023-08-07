95774fc91c614d27478b499b32190390dac21013
ChemaJob <chema.job.24@gmail.com>
2023-08-07 11:00:51 -0600

    Administracion de pacientes - Leyendo la información escrita en un Input y colocandola en el State

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,10 +1,7 @@
 import { useState, useEffect } from "react";
 
 const Formulario = () => {
-
-  const Formulario = () => {
     const [nombre, setNombre] = useState('');
-  }
 
   return (
     <div className="md:w-1/2 lg:w-2/5">
@@ -15,14 +12,16 @@ const Formulario = () => {
         <span className="text-indigo-600 font-bold">Administralos</span>
       </p>
 
-      <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+      <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">g
         <div className="mb-5">
           <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">Nombre Mascota</label>
           <input 
           id="mascota"
           type="text" 
           placeholder="Nombre de la mascota" 
-          className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" />
+          className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          value={nombre}
+          onChange={(e) => setNombre(e.target.value)} />
         </div>
 
         <div className="mb-5">
