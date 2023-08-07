88732696fc81ff70580fb82e6171dcda233e125e
ChemaJob <chema.job.24@gmail.com>
2023-08-07 13:05:24 -0600

    Administracion de pacientes - iterando sobre un Array en Reac

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,6 +1,6 @@
 import Pacientes from "./Pacientes";
 
-const ListadoPacientes = () => {
+const ListadoPacientes = ({pacientes}) => {
   return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
       <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
@@ -8,12 +8,13 @@ const ListadoPacientes = () => {
         Administra tus {""}
         <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
       </p>
-      <Pacientes />
-      <Pacientes />
-      <Pacientes />
-      <Pacientes />
-      <Pacientes />
-      <Pacientes />
+
+      { pacientes.map( paciente => (
+        <Pacientes 
+          paciente={paciente}
+        />
+      ))}
+      
     </div>
   );
 };
