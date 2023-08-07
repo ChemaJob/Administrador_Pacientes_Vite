1381620a664d410bcac16d66472eda9beaa798ac
ChemaJob <chema.job.24@gmail.com>
2023-08-07 10:30:01 -0600

    Administracion de pacientes - Creando el Listado de Pacientes

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -4,7 +4,7 @@ const Formulario = () => {
     <div className="md:w-1/2 lg:w-2/5">
       <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
 
-      <p className="text-lg mt-5 text-center">
+      <p className="text-lg mt-5 text-center mb-10">
         Añade Pacientes y {''}
         <span className="text-indigo-600 font-bold">Administralos</span>
       </p>
---
+++ src/components/Pacientes.jsx
@@ -0,0 +1,37 @@
+const Pacientes = () => {
+  return (
+    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Nombre: {""}
+        <span className="font-normal normal-case">Hook</span>
+      </p>
+
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Propietario: {""}
+        <span className="font-normal normal-case">Chema</span>
+      </p>
+
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        e-mail: {""}
+        <span className="font-normal normal-case">user@mail.com</span>
+      </p>
+
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Fecha alta: {""}
+        <span className="font-normal normal-case">10 de octubre de 2022</span>
+      </p>
+
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Síntomas: {""}
+        <span className="font-normal normal-case">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic, dicta
+          aspernatur tempore, ut animi sunt officia cupiditate blanditiis sit
+          ipsam quia, eos doloremque pariatur nobis praesentium dignissimos
+          nulla qui iusto.
+        </span>
+      </p>
+    </div>
+  );
+};
+
+export default Pacientes;
